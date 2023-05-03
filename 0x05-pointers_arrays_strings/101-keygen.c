457a37d466a339e0f84b195ccf88ae7a831038bc
natashaa <natashamathapelo12@gmail.com>
2023-05-02 18:25:59 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -26,7 +26,7 @@ int main(void)
 	diff_half1 = (sum - 2772) / 2;
 	diff_half2 = (sum - 2772) / 2;
 	if ((sum - 2772) % 2 != 0)
-	diff_half++;
+	diff_half1++;
 	for (index = 0; password[index]; index++)
 	{
 	if (password[index] >= (33 + diff_half1))
@@ -39,7 +39,7 @@ int main(void)
 	{
 	if (password[index] >= (33 + diff_half2))
 	{
-	passwods[index] -= diff_half2;
+	password[index] -= diff_half2;
 	break;
 	}
 	}
