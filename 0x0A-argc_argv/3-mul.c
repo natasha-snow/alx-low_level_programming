d0957b3a2f5d26a51e55fd17f91c72c920f352db
natashaa <natashamathapelo12@gmail.com>
2023-05-16 04:42:24 -0700

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
+		n1 = _atoi(argv[1]);
+		n2 = _atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	else
